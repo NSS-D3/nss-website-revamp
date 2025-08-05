ee73c0b2b24f3cc7ac4aba693cbfa78579d01e91
Tanmay <tanmayarora382006@gmail.com>
2025-08-05 14:21:03 +0530

    changed to office bearers

--- src/components/team-section.tsx
+++ src/components/team-section.tsx
@@ -156,10 +156,10 @@ export function TeamSection() {
           )}
         </div>
 
-        {/* Leadership Team */}
+        {/* Office Bearers */}
         <div className="mb-16">
           <h3 className="text-2xl font-bold text-center text-gray-900 mb-8">
-            Leadership Team
+            Office Bearers
           </h3>
 
           {leadershipTeam.length > 0 && (
